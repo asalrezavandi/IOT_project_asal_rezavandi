d51363c42537c08c29f162e1670f781f1304f6c7
asalrezavandi <asalrezavandi2004@gmail.com>
2025-10-08 19:20:39 +0330

    device.py

--- device.py
+++ device.py
@@ -45,11 +45,11 @@ class Device:
                 GPIO.setup(38,GPIO.OUT)
 
 else:
-print('نوع دستگاه ناشناخته است'):
+print('decive type not defined'):
             
 
     def turn_on(self):
-        print(f'دستگاه{self.device_name}({self.device_type})روشن شد')
+        print('turn on',self.device_name)
         self.status='on'
         #oon devicer --> SHERKAT vasl bshe --> dastoopr bde --> sherkate b oon lampe vasl bshe
         #va oon lamp baram 'ROSHAN' kone
@@ -59,7 +59,7 @@ print('نوع دستگاه ناشناخته است'):
         
 
     def turn_off(self):
-        print(f'دستگاه{self.device_name}({self.device_type})خاموش شد')
+        print('turn off',self.device_name)
         self.status='off'
         #bayad inja bnvis-->sherkate begam agah in device 
         #shekrat elamp --> 'Khamoosh' kone
@@ -76,62 +76,9 @@ print('نوع دستگاه ناشناخته است'):
         
 #a1=..... (class --> device az tarighe ketabkhone)
 
-a1=Device('home','room','lights','lamps1001','w328376231863816326216')
+a1=Device('home','room1','lights','camera','camera1001',38 )
 
 a1.turn_on()
 
 a1.turn_off()
 
-
-
-class Sensor:
-    
-    def __init__(self,location,group,sensor_name,sensor_type,pin):
-        self.location=location
-        self.group=group
-        self.sensor_name=sensor_name
-        self.sensor_type=sensor_type
-        
-        self.pin=pin
-        
-        
-a1=Sensor('hom','room1','temp','thermoset10','2823shasjash')
-
-
-a1.location
-
-a1.pin
-
-
-#a1.turn_on()
-
-#**things -_> device / sensor
-#devcie -->dastoor turn on , off
-
-
-#sensor--> damaor , begiri data -->: read_data()
-
-
-    
-import Adafruiy_DHT   
-    
-class Sensor:
-    
-    def __init__(self,location,group,sensor_name,sensor_type,pin):
-        self.location=location
-        self.group=group
-        self.sensor_name=sensor_name
-        self.sensor_type=sensor_type
-        
-        self.pin=pin
-        
-        
-    def read_data(self):
-        humidity,temeprature=Adafruiy_DHT.read_retry(Adafruiy_DHT.DHT22,self.pin)
-        
-        return temeprature
-        
-a1=Sensor('hom','room1','temp','thermoset10','2823shasjash')
-
-
-a1.read_data()      
